b09f1f8cab988adf16fdd47f71e0d358e8825007
Torben Zurhelle <zurhelle@outlook.com>
2016-09-23 14:09:20 +0200

    visit prop

--- Data/Expr/Proposition/Visit.hs
+++ Data/Expr/Proposition/Visit.hs
@@ -1,7 +1,6 @@
 -- | the visitor pattern for Expr
 
 module Data.Expr.Proposition.Visit where
-
 import Data.Expr.Proposition.Types
 
 -- ----------------------------------------
@@ -18,6 +17,11 @@ idExpr
   = undefined
 
 visit :: Visitor r -> Expr -> r
-visit v e = undefined
+visit v (Lit b) = vLit v b
+visit v (Var name) = vVar v name
+visit v (Unary op e) = (vUnary v) op $ visit v e
+visit v (Binary op expr1 expr2) = (vBinary v) op (visit v expr1) (visit v expr2)
+
+
 
 -- ----------------------------------------
