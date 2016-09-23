ed03ebe65f33d3accabc427389544b2a94c96fb4
Philipp Kähler <p.kaehler@mytaxi.com>
2016-09-23 15:45:29 +0200

    substvars

--- Data/Expr/Proposition/Visitors.hs
+++ Data/Expr/Proposition/Visitors.hs
@@ -19,7 +19,7 @@ freeVars
     V {vLit   = \b -> S.empty
     , vVar    = \i -> S.singleton i
     , vUnary  = \op1 k1 -> k1
-    , vBinary = \op2 k2 k3 -> S.union k2 k3 
+    , vBinary = \op2 k2 k3 -> S.union k2 k3
     }
 
 type VarEnv = [(Ident, Expr)]
@@ -27,7 +27,13 @@ type VarEnv = [(Ident, Expr)]
 substVars :: VarEnv -> Expr -> Expr
 substVars env
   = visit $
-    undefined
+      V {vLit   = Lit
+      , vVar    = \x -> case lookup x env of
+                        Nothing -> Var x
+                        Just a -> a
+      , vUnary  = Unary
+      , vBinary = Binary
+      }
 
 eval :: Expr -> Bool
 eval
