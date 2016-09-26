4307cef11f778b38dc86c1b8f9bb24864e84014a
Torben Zurhelle <zurhelle@outlook.com>
2016-09-26 14:23:08 +0200

    FunktionalList

--- Data/FunctionalList.hs
+++ Data/FunctionalList.hs
@@ -9,52 +9,52 @@ type List a = [a] -> [a]
 -- ----------------------------------------
 
 fromList        :: [a] -> List a
-fromList l      = undefined
+fromList l      = (\xs -> xs ++ l)
 
 toList          :: List a -> [a]
-toList l        = undefined
+toList l        =l []
 
 empty           :: List a
-empty           = undefined
+empty           =P.id
 
 singleton       :: a -> List a
-singleton e     = undefined
+singleton e     = fromList . (:[])
 
 -- (:) for functional lists
 cons            :: a -> List a -> List a
-cons e l        = undefined
+cons e l        = (e:) . l
 
 -- dual to cons
 snoc            :: List a -> a -> List a
-snoc l e        = undefined
+snoc l e        = (++[e]) . l
 
 -- (++) for functional lists
 append          :: List a -> List a -> List a
-append l1 l2    = undefined
+append l1 l2    = (.)
 
 -- like concat for normal lists: foldr (++) []
 concat          :: [List a] -> List a
-concat          = undefined
+concat          =P.foldr append empty
 
 -- like map for normal lists: foldr ((:) . f) []
 map             :: (a -> b) -> List a -> List b
-map f           = undefined
+map f           = (++) . P.map f . toList
 
 -- foldr with foldr for normal lists
 foldr           :: (a -> b -> b) -> b -> List a -> b
-foldr op n      = undefined
+foldr op n      = P.foldr op n . toList
 
 -- head, tail, null
 head            :: List a -> a
-head            = undefined
+head            = P.head . toList 
 
 tail            :: List a -> List a
-tail            = undefined
+tail            = P.tail .l
 
 null            :: List a -> Bool
-null            = undefined
+null            =P.null . toList
 
 reverse         :: List a -> List a
-reverse         = undefined
+reverse         = p.reverse.l
 
 -- ----------------------------------------
